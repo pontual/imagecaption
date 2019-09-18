cedfbaac567a69babc20b42d1410edaa26a487b3
Heitor Chang <heitorchang@gmail.com>
2019-09-18 15:56:33 -0300

    flatten dirs

--- caption.py
+++ caption.py
@@ -12,6 +12,7 @@ def caption(origfilename,
     font = ImageFont.truetype("c:/Users/Heitor/Desktop/code/imagecaption/ubuntu.ttf", fontsize)
     draw.text((10, 10), text, (190, 0, 0), font=font)
 
-    target_filename = origfilename.split("imagecaption/original/")[-1]
+    # target_filename = origfilename.split("imagecaption/original/")[-1]
+    target_filename = origfilename.split("/")[-1]
     
     img.save("c:/Users/Heitor/Desktop/code/imagecaption/anotado/" + target_filename)
